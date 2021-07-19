16657e604662b9c03c46d2a6544789e7e753a989
karlostiago <carlostiagodesousa@gmail.com>
2021-07-18 23:49:34 -0300

    transformando classe AbstractApplicationTest em abstract

--- src/test/java/com/ctsousa/stoom/controller/AbstractApplicationTest.java
+++ src/test/java/com/ctsousa/stoom/controller/AbstractApplicationTest.java
@@ -9,7 +9,7 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @TestPropertySource("/application-test.properties")
-public class AbstractApplicationTest {
+public abstract class AbstractApplicationTest {
 	
 	@LocalServerPort
 	protected int port;
